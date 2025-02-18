de4193a1507df79e0dbfd8784ba02b9dde90e018
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2025-02-18 08:40:36 +0530

    Add I Am Rich App

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dwarakanatha Reddy Poreddy on 18/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
