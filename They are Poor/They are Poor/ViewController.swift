785a00e24f3c29c00742bebcbaf24077fc8e5709
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2025-02-19 07:38:27 +0530

    Add They Are Poor

---
+++ They are Poor/They are Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  They are Poor
+//
+//  Created by Dwarakanatha Reddy Poreddy on 19/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
