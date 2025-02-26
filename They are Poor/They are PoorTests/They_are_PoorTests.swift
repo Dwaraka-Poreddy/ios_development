c8cf0769528a90bb5fa402603f8aef45826842bb
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2025-02-26 20:13:05 +0530

    Update repository by removing submodules

---
+++ They are Poor/They are PoorTests/They_are_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  They_are_PoorTests.swift
+//  They are PoorTests
+//
+//  Created by Dwarakanatha Reddy Poreddy on 19/02/25.
+//
+
+import Testing
+@testable import They_are_Poor
+
+struct They_are_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ They are Poor/They are PoorUITests/They_are_PoorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  They_are_PoorUITestsLaunchTests.swift
+//  They are PoorUITests
+//
+//  Created by Dwarakanatha Reddy Poreddy on 19/02/25.
+//
+
+import XCTest
+
+final class They_are_PoorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
