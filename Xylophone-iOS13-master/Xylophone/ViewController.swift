e7bb07efae1ec34e72a883250bd6b53c914a2938
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2025-02-28 08:48:23 +0530

    Finish Xylophone base project

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(name: sender.currentTitle ?? "C")
+        sender.layer.opacity = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.layer.opacity = 1
+        }
+    }
 
+    func playSound(name: String) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+    
 }
 
